de970051616b7e11678c0eb6ee997825a5fbac8e
GonMarinas <157437596+GonMarinas@users.noreply.github.com>
2025-06-13 11:32:05 +0200

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -35,4 +35,6 @@ def create_invite():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Toma el puerto de Render o 5000 localmente
+    app.run(host='0.0.0.0', port=port)
+
